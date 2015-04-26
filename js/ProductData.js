878cecdab2ac3abf77c38efe9fd15bf93c4c3713
JardarNesse <jardar.nesse@frende.no>
2015-04-26 07:18:26 +0200

    Initial version
    Added initial shopping cart demo

---
+++ js/ProductData.js
@@ -0,0 +1,36 @@
+module.exports = {
+  // Load Mock Product Data Into localStorage
+  init: function() {
+    localStorage.clear();
+    localStorage.setItem('product', JSON.stringify([
+      {
+        id: '0011001',
+        name: 'Forsikring',
+        image: 'forsikringer.png',
+        description: 'Velg blandt våre forsikringer',
+        variants: [
+          {
+            sku: '123123',
+            type: 'Reise',
+            price: 250.00,
+            inventory: 1
+
+          },
+          {
+            sku: '123124',
+            type: 'Ulykke',
+            price: 300.00,
+            inventory: 1
+          },
+          {
+            sku: '1231235',
+            type: 'Bolig',
+            price: 800.00,
+            inventory: 1
+          }
+        ]
+      }
+    ]));
+  }
+
+};
